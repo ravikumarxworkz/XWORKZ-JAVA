49695cce38c8183b9b925d3a1a9d83055fb583f9
Ravikumar <ravikumar.xworkz@gmail.com>
2023-10-27 18:58:38 +0530

    task 26-10-23

---
+++ task 26-10-23/GetterSetter.java
@@ -0,0 +1,447 @@
+package com.things;
+public class GetterSetter {
+private String name;
+private int   id;
+private int    employeeId;
+private char  sex;
+private long   phoneNumber;
+private float   height;
+private double  weight;
+private boolean degreeompleted;
+private int   numOfFamileMembers;
+private String fatherName;
+private String motherName;
+private String adress;
+private int    pinCode;
+private String myCarName;
+private String carNumber;
+private String bikeName;
+private String bikeNumber;
+private String favoriteFood;
+private String nickName;
+private String myCompanyName;
+private int    luckyNumber;
+private String favoritHero;
+private String favoritheroni;
+private String favoritegame;
+private String favoriteSportPerson;
+private String favoriteMovie ;
+private String favoriteseries;
+private String SchoolName;
+private double tenthpercentage;
+private String pucName;
+private double pucPercentage;
+private String collegeName;
+private String usnNumber;
+private double degreePercentage;
+private String myVillageName;
+private String talukName;
+private String districtName;
+private String StateName;
+private String countryName;
+private float myFootSize;
+private String myLaptopName;
+private String myMobileName ;
+private int numbeerOfContact;
+private String favoriteIpleTeam ;
+private String  favoritepalace;
+private String favoritedrink ;
+private int numberOFriends ;
+private int numberOfBikes ;
+private String dob ;
+private String aim ;
+private String myHobbie ;
+public GetterSetter(String name, int i, int employeeId, char c, long phoneNumber, float height, double weight,
+		boolean degreeompleted, int j, String fatherName, String motherName, String adress,
+		int pinCode, String myCarName, String carNumber, String bikeName, String bikeNumber, String favoriteFood,
+		String nickName, String myCompanyName, int luckyNumber, String favoritHero, String favoritheroni,
+		String favoritegame, String favoriteSportPerson, String favoriteMovie, String favoriteseries, String schoolName,
+		double tenthpercentage, String pucName, double pucPercentage, String collegeName, String usnNumber,
+		double degreePercentage, String myVillageName, String talukName, String districtName, String stateName,
+		String countryName, float myFootSize, String myLaptopName, String myMobileName, int numbeerOfContact,
+		String favoriteIpleTeam, String favoritepalace, String favoritedrink, int numberOFriends, int numberOfBikes,
+		String dob, String aim, String myHobbie) {
+	this.name = name;
+	this.id = i;
+	this.employeeId = employeeId;
+	this.sex = c;
+	this.phoneNumber = phoneNumber;
+	this.height = height;
+	this.weight = weight;
+	this.degreeompleted = degreeompleted;
+	this.numOfFamileMembers = j;
+	this.fatherName = fatherName;
+	this.motherName = motherName;
+	this.adress = adress;
+	this.pinCode = pinCode;
+	this.myCarName = myCarName;
+	this.carNumber = carNumber;
+	this.bikeName = bikeName;
+	this.bikeNumber = bikeNumber;
+	this.favoriteFood = favoriteFood;
+	this.nickName = nickName;
+	this.myCompanyName = myCompanyName;
+	this.luckyNumber = luckyNumber;
+	this.favoritHero = favoritHero;
+	this.favoritheroni = favoritheroni;
+	this.favoritegame = favoritegame;
+	this.favoriteSportPerson = favoriteSportPerson;
+	this.favoriteMovie = favoriteMovie;
+	this.favoriteseries = favoriteseries;
+	SchoolName = schoolName;
+	this.tenthpercentage = tenthpercentage;
+	this.pucName = pucName;
+	this.pucPercentage = pucPercentage;
+	this.collegeName = collegeName;
+	this.usnNumber = usnNumber;
+	this.degreePercentage = degreePercentage;
+	this.myVillageName = myVillageName;
+	this.talukName = talukName;
+	this.districtName = districtName;
+	StateName = stateName;
+	this.countryName = countryName;
+	this.myFootSize = myFootSize;
+	this.myLaptopName = myLaptopName;
+	this.myMobileName = myMobileName;
+	this.numbeerOfContact = numbeerOfContact;
+	this.favoriteIpleTeam = favoriteIpleTeam;
+	this.favoritepalace = favoritepalace;
+	this.favoritedrink = favoritedrink;
+	this.numberOFriends = numberOFriends;
+	this.numberOfBikes = numberOfBikes;
+	this.dob = dob;
+	this.aim = aim;
+	this.myHobbie = myHobbie;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getId() {
+	return id;
+}
+public void setId(byte id) {
+	this.id = id;
+}
+public int getEmployeeId() {
+	return employeeId;
+}
+public void setEmployeeId(int employeeId) {
+	this.employeeId = employeeId;
+}
+public char getSex() {
+	return sex;
+}
+public void setSex(char sex) {
+	this.sex = sex;
+}
+public long getPhoneNumber() {
+	return phoneNumber;
+}
+public void setPhoneNumber(long phoneNumber) {
+	this.phoneNumber = phoneNumber;
+}
+public float getHeight() {
+	return height;
+}
+public void setHeight(float height) {
+	this.height = height;
+}
+public double getWeight() {
+	return weight;
+}
+public void setWeight(double weight) {
+	this.weight = weight;
+}
+public boolean isDegreeompleted() {
+	return degreeompleted;
+}
+public void setDegreeompleted(boolean degreeompleted) {
+	this.degreeompleted = degreeompleted;
+}
+public int getNumOfFamileMembers() {
+	return numOfFamileMembers;
+}
+public void setNumOfFamileMembers(byte numOfFamileMembers) {
+	this.numOfFamileMembers = numOfFamileMembers;
+}
+public String getFatherName() {
+	return fatherName;
+}
+public void setFatherName(String fatherName) {
+	this.fatherName = fatherName;
+}
+public String getMotherName() {
+	return motherName;
+}
+public void setMotherName(String motherName) {
+	this.motherName = motherName;
+}
+public String getAdress() {
+	return adress;
+}
+public void setAdress(String adress) {
+	this.adress = adress;
+}
+public int getPinCode() {
+	return pinCode;
+}
+public void setPinCode(int pinCode) {
+	this.pinCode = pinCode;
+}
+public String getMyCarName() {
+	return myCarName;
+}
+public void setMyCarName(String myCarName) {
+	this.myCarName = myCarName;
+}
+public String getCarNumber() {
+	return carNumber;
+}
+public void setCarNumber(String carNumber) {
+	this.carNumber = carNumber;
+}
+public String getBikeName() {
+	return bikeName;
+}
+public void setBikeName(String bikeName) {
+	this.bikeName = bikeName;
+}
+public String getBikeNumber() {
+	return bikeNumber;
+}
+public void setBikeNumber(String bikeNumber) {
+	this.bikeNumber = bikeNumber;
+}
+public String getFavoriteFood() {
+	return favoriteFood;
+}
+public void setFavoriteFood(String favoriteFood) {
+	this.favoriteFood = favoriteFood;
+}
+public String getNickName() {
+	return nickName;
+}
+public void setNickName(String nickName) {
+	this.nickName = nickName;
+}
+public String getMyCompanyName() {
+	return myCompanyName;
+}
+public void setMyCompanyName(String myCompanyName) {
+	this.myCompanyName = myCompanyName;
+}
+public int getLuckyNumber() {
+	return luckyNumber;
+}
+public void setLuckyNumber(int luckyNumber) {
+	this.luckyNumber = luckyNumber;
+}
+public String getFavoritHero() {
+	return favoritHero;
+}
+public void setFavoritHero(String favoritHero) {
+	this.favoritHero = favoritHero;
+}
+public String getFavoritheroni() {
+	return favoritheroni;
+}
+public void setFavoritheroni(String favoritheroni) {
+	this.favoritheroni = favoritheroni;
+}
+public String getFavoritegame() {
+	return favoritegame;
+}
+public void setFavoritegame(String favoritegame) {
+	this.favoritegame = favoritegame;
+}
+public String getFavoriteSportPerson() {
+	return favoriteSportPerson;
+}
+public void setFavoriteSportPerson(String favoriteSportPerson) {
+	this.favoriteSportPerson = favoriteSportPerson;
+}
+public String getFavoriteMovie() {
+	return favoriteMovie;
+}
+public void setFavoriteMovie(String favoriteMovie) {
+	this.favoriteMovie = favoriteMovie;
+}
+public String getFavoriteseries() {
+	return favoriteseries;
+}
+public void setFavoriteseries(String favoriteseries) {
+	this.favoriteseries = favoriteseries;
+}
+public String getSchoolName() {
+	return SchoolName;
+}
+public void setSchoolName(String schoolName) {
+	SchoolName = schoolName;
+}
+public double getTenthpercentage() {
+	return tenthpercentage;
+}
+public void setTenthpercentage(double tenthpercentage) {
+	this.tenthpercentage = tenthpercentage;
+}
+public String getPucName() {
+	return pucName;
+}
+public void setPucName(String pucName) {
+	this.pucName = pucName;
+}
+public double getPucPercentage() {
+	return pucPercentage;
+}
+public void setPucPercentage(double pucPercentage) {
+	this.pucPercentage = pucPercentage;
+}
+public String getCollegeName() {
+	return collegeName;
+}
+public void setCollegeName(String collegeName) {
+	this.collegeName = collegeName;
+}
+public String getUsnNumber() {
+	return usnNumber;
+}
+public void setUsnNumber(String usnNumber) {
+	this.usnNumber = usnNumber;
+}
+public double getDegreePercentage() {
+	return degreePercentage;
+}
+public void setDegreePercentage(double degreePercentage) {
+	this.degreePercentage = degreePercentage;
+}
+public String getMyVillageName() {
+	return myVillageName;
+}
+public void setMyVillageName(String myVillageName) {
+	this.myVillageName = myVillageName;
+}
+public String getTalukName() {
+	return talukName;
+}
+public void setTalukName(String talukName) {
+	this.talukName = talukName;
+}
+public String getDistrictName() {
+	return districtName;
+}
+public void setDistrictName(String districtName) {
+	this.districtName = districtName;
+}
+public String getStateName() {
+	return StateName;
+}
+public void setStateName(String stateName) {
+	StateName = stateName;
+}
+public String getCountryName() {
+	return countryName;
+}
+public void setCountryName(String countryName) {
+	this.countryName = countryName;
+}
+public float getMyFootSize() {
+	return myFootSize;
+}
+public void setMyFootSize(float myFootSize) {
+	this.myFootSize = myFootSize;
+}
+public String getMyLaptopName() {
+	return myLaptopName;
+}
+public void setMyLaptopName(String myLaptopName) {
+	this.myLaptopName = myLaptopName;
+}
+public String getMyMobileName() {
+	return myMobileName;
+}
+public void setMyMobileName(String myMobileName) {
+	this.myMobileName = myMobileName;
+}
+public int getNumbeerOfContact() {
+	return numbeerOfContact;
+}
+public void setNumbeerOfContact(int numbeerOfContact) {
+	this.numbeerOfContact = numbeerOfContact;
+}
+public String getFavoriteIpleTeam() {
+	return favoriteIpleTeam;
+}
+public void setFavoriteIpleTeam(String favoriteIpleTeam) {
+	this.favoriteIpleTeam = favoriteIpleTeam;
+}
+public String getFavoritepalace() {
+	return favoritepalace;
+}
+public void setFavoritepalace(String favoritepalace) {
+	this.favoritepalace = favoritepalace;
+}
+public String getFavoritedrink() {
+	return favoritedrink;
+}
+public void setFavoritedrink(String favoritedrink) {
+	this.favoritedrink = favoritedrink;
+}
+public int getNumberOFriends() {
+	return numberOFriends;
+}
+public void setNumberOFriends(int numberOFriends) {
+	this.numberOFriends = numberOFriends;
+}
+public int getNumberOfBikes() {
+	return numberOfBikes;
+}
+public void setNumberOfBikes(int numberOfBikes) {
+	this.numberOfBikes = numberOfBikes;
+}
+public String getDob() {
+	return dob;
+}
+public void setDob(String dob) {
+	this.dob = dob;
+}
+public String getAim() {
+	return aim;
+}
+public void setAim(String aim) {
+	this.aim = aim;
+}
+public String getMyHobbie() {
+	return myHobbie;
+}
+public void setMyHobbie(String myHobbie) {
+	this.myHobbie = myHobbie;
+}
+@Override
+public String toString() {
+	return "GetterSetter [name=" + name + ", id=" + id + ", employeeId=" + employeeId + ", sex=" + sex
+			+ ", phoneNumber=" + phoneNumber + ", height=" + height + ", weight=" + weight + ", degreeompleted="
+			+ degreeompleted + ", numOfFamileMembers=" + numOfFamileMembers + ", fatherName=" + fatherName
+			+ ", motherName=" + motherName + ", adress=" + adress + ", pinCode=" + pinCode + ", myCarName=" + myCarName
+			+ ", carNumber=" + carNumber + ", bikeName=" + bikeName + ", bikeNumber=" + bikeNumber + ", favoriteFood="
+			+ favoriteFood + ", nickName=" + nickName + ", myCompanyName=" + myCompanyName + ", luckyNumber="
+			+ luckyNumber + ", favoritHero=" + favoritHero + ", favoritheroni=" + favoritheroni + ", favoritegame="
+			+ favoritegame + ", favoriteSportPerson=" + favoriteSportPerson + ", favoriteMovie=" + favoriteMovie
+			+ ", favoriteseries=" + favoriteseries + ", SchoolName=" + SchoolName + ", tenthpercentage="
+			+ tenthpercentage + ", pucName=" + pucName + ", pucPercentage=" + pucPercentage + ", collegeName="
+			+ collegeName + ", usnNumber=" + usnNumber + ", degreePercentage=" + degreePercentage + ", myVillageName="
+			+ myVillageName + ", talukName=" + talukName + ", districtName=" + districtName + ", StateName=" + StateName
+			+ ", countryName=" + countryName + ", myFootSize=" + myFootSize + ", myLaptopName=" + myLaptopName
+			+ ", myMobileName=" + myMobileName + ", numbeerOfContact=" + numbeerOfContact + ", favoriteIpleTeam="
+			+ favoriteIpleTeam + ", favoritepalace=" + favoritepalace + ", favoritedrink=" + favoritedrink
+			+ ", numberOFriends=" + numberOFriends + ", numberOfBikes=" + numberOfBikes + ", dob=" + dob + ", aim="
+			+ aim + ", myHobbie=" + myHobbie + "]";
+}
+
+
+
+
+
+}
---
+++ task 26-10-23/GetterSetterR.java
@@ -0,0 +1,21 @@
+package com.driver;
+
+import com.things.GetterSetter;
+
+public class GetterSetterR {
+
+	public static void main(String[] args) {
+		GetterSetter g=new GetterSetter("ravikuamr",100,582415,'M',8123179157l,5.8f,75,
+				true,5,"shankar","mahanada","s/o shankar kumbar uagr budruk karnataka*591316",
+				591316,"audi","MH15GF3588","shine","KA23AEET1193","JAMUNA",
+				"KARNTI","IT",17,"DHARSHAN","MANJUALA",
+				"CRICKET","VIRATKHOLI","UGRAM","JANTRA","JET",
+				82.40,"PRISM",82.40,"SMVIT","1MV16ME077",
+				7.2,"ugarbudruk","kagwad","belagavi","karnataka",
+				"india",9.5f,"hp","realme",123,
+				"rcb","myhome","monster",10,2,
+				"1/1/1997","PSI","reaingBooks");
+		System.out.println(g.toString());
+	}
+
+}
