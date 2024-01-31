61b23c004ed003fbbdf276ec6f2493bfac5cb5ff
RAVIKUMAR SHANNKAR KUMBAR <147067056+ravikumarxworkz@users.noreply.github.com>
2024-01-31 14:10:55 +0530

    stack

---
+++ mountains/src/com/xworkz/mountain/runner/MountainRunner.java
@@ -0,0 +1,55 @@
+package com.xworkz.mountain.runner;
+
+import java.util.Stack;
+
+import com.xworkz.mountains.dto.MountainDto;
+
+public class MountainRunner {
+
+	public static void main(String[] args) {
+		MountainDto dto = new MountainDto("K2", 8611, 340, "INDIA");
+		MountainDto dto1 = new MountainDto("MT Everst", 8848, 350, "NEPAL-CHINA");
+		MountainDto dto2 = new MountainDto("ALPHA", 420, 80, "canada");
+		MountainDto dto3 = new MountainDto("ROCKY mountain", 4500, 150, "north america");
+		MountainDto dto4 = new MountainDto("muliyana giri", 1693, 10, "india");
+		MountainDto dto5 = new MountainDto("javed hills", 1493, 10, "india");
+		MountainDto dto6 = new MountainDto("javed hills", 1493, 10, "india");
+
+		Stack<MountainDto> stack = new Stack<MountainDto>();
+
+		stack.add(dto);
+		stack.add(dto1);
+		stack.add(dto2);
+		stack.add(dto3);
+		stack.push(dto4);///////////////////////////////////////////1
+		stack.addElement(dto5);
+		for (MountainDto mountainDto : stack) {
+			System.out.println(mountainDto);
+		}
+		System.out.println("================================");
+		System.out.println(stack.elementAt(2));
+		System.out.println("================================");
+
+		System.out.println("delete all=" + stack.pop());///////////////////2
+		System.out.println("================================");
+
+		for (MountainDto mountainDto : stack) {
+			System.out.println(mountainDto);
+		}
+		System.out.println("================================");
+		System.out.println("peek=" + stack.peek());/////////////////3
+		System.out.println("================================");
+		for (MountainDto mountainDto : stack) {
+			System.out.println(mountainDto);
+		}
+		System.out.println("================================");
+
+		System.out.println("empty method=" + stack.empty());///////////4
+
+		System.out.println("================================");
+		
+		System.out.println("searching object"+stack.search(dto6));///////////5
+
+	}
+
+}
